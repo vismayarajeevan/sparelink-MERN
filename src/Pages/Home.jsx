190d964b248af81275764aa877ea6af514bd0b8a
vismayarajeevan <vismayarajeevan15@gmail.com>
2025-02-06 10:05:29 +0530

    imageslider completed with responsive design

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../Components/Header'
+import ImageSlider from '../Components/ImageSlider'
 
 const Home = () => {
   return (
   <>
   <Header />
+  <ImageSlider />
   </>
  
   )
